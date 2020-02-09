49c89d652834c48c4c1cbf61f9fae8c103b697db
t4arty <t4arty@yandex.ru>
2020-02-09 23:32:44 +0300

    work but not gread,error if already exist folder

--- test_csv.py
+++ test_csv.py
@@ -1,14 +1,20 @@
 import os
 import sys
+import subprocess
 
 lis = list()
-pth = ""
+mainpath = "d:\\from_linux\\"
+reposlistfile = mainpath + "repos_task.txt"
 
-with open('repos_task.txt','r',encoding='utf-8') as l:
+if (os.path.exists(mainpath) == False):
+    os.mkdir(mainpath)
+
+with open(reposlistfile,'r',encoding='utf-8') as l:
     lis = l.readlines()
 
 for x in lis:
     rep = str(x).replace("https://","").strip()
     calList = rep.split("/")
-    if (os.path.exists('/opt/repos'+calList[len(calList)-1]) == False):
-        os.mkdir('/opt/repos'+calList[len(calList)-1])
+    subprocess.call(["git","--bare","clone", x.strip(), mainpath+calList[2]])
+    calList = list()
+
